a7238e57633851d5daba4654a8aae4db6dbd507d
Rüzgar Mehmet Deniz <ruzgarmehmetdeniz@gmail.com>
2026-05-07 16:13:22 +0300

    Update DiscountObserver.cs

--- DesignPatterns/DesignPatterns/Observer/DiscountObserver.cs
+++ DesignPatterns/DesignPatterns/Observer/DiscountObserver.cs
@@ -5,21 +5,19 @@ namespace DesignPatterns.DesignPatterns.Observer
     public class DiscountObserver : IObserver
     {
         public void CreateObserver(CustomerProcess process)
-        {
-            decimal eskiFiyat = process.Amount;
-            decimal yeniFiyat = eskiFiyat / 2;
-
-            string mesaj = $"📢 MÜJDE: Sayın {process.CustomerName}, beklediğiniz fırsat geldi!\n" +
-                           $"🥒 Çengelköy Salatalık ürününde %50 dev indirim başladı!\n" +
-                           $"❌ Eski Fiyat: {eskiFiyat:C2}\n" +
-                           $"✅ İndirimli Fiyat: {yeniFiyat:C2}\n" +
-                           $"Kaçırmamak için hemen organikmarket.com üzerinden sipariş verin! 🌿";
-
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine($"[BİLDİRİM GÖNDERİLDİ] -> {process.CustomerName}");
-            Console.WriteLine("MESAJ İÇERİĞİ:");
-            Console.WriteLine(mesaj);
-            Console.WriteLine("-----------------------------------------------");
-        }
+{
+    decimal eskiFiyat = process.Product.Price;  
+    decimal yeniFiyat = eskiFiyat / 2;
+    string mesaj = $"📢 MÜJDE: Sayın {process.CustomerName}, beklediğiniz fırsat geldi!\n" +
+                   $"🥒 {process.Product.Name} ürününde %50 dev indirim başladı!\n" +
+                   $"❌ Eski Fiyat: {eskiFiyat:C2}\n" +
+                   $"✅ İndirimli Fiyat: {yeniFiyat:C2}\n" +
+                   $"Kaçırmamak için hemen organikmarket.com üzerinden sipariş verin! 🌿";
+    Console.WriteLine("-----------------------------------------------");
+    Console.WriteLine($"[BİLDİRİM GÖNDERİLDİ] -> {process.CustomerName}");
+    Console.WriteLine("MESAJ İÇERİĞİ:");
+    Console.WriteLine(mesaj);
+    Console.WriteLine("-----------------------------------------------");
+}
     }
 }
